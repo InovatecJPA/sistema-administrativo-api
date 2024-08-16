ee066905dd665ec93f4f8d7f76fdd53cee022cf3
ArthurRamonSouza <arthuramon.souza93@hotmail.com>
2024-08-16 11:28:23 -0300

    refactor: apagando comentários

--- src/database/dbConnection.ts
+++ src/database/dbConnection.ts
@@ -1,18 +1,14 @@
 import { dbConfig } from "../config/dbConfig";
 import { DataSource } from "typeorm";
 
-// (Sequelize as any).postgres.DECIMAL.parse = function (value: any): number {
-// 	return parseFloat(value);
-// };
-
 const AppDataSource = new DataSource(dbConfig);
 
 AppDataSource.initialize()
-	.then(() => {
-		console.log("Conexão com o banco de dados estabelecida com sucesso!");
-	})
-	.catch((error) => {
-		console.error("Erro ao conectar com o banco de dados", error);
-	});
+  .then(() => {
+    console.log("Conexão com o banco de dados estabelecida com sucesso!");
+  })
+  .catch((error) => {
+    console.error("Erro ao conectar com o banco de dados", error);
+  });
 
 export default AppDataSource;
