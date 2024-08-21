ba6125a14e42ae8c97ec433b87d46e7c91213d4f
ArthurRamonSouza <arthuramon.souza93@hotmail.com>
2024-08-21 12:48:10 -0300

    refactor(UserController): adicionando mais validações e tratamento de erros

--- src/apps/Auth/controller/UserController.ts
+++ src/apps/Auth/controller/UserController.ts
@@ -1,12 +1,12 @@
-import Profile from "../model/Profile";
-import User from "../model/User";
-
 import { Request, Response } from "express";
-import AppDataSource from "../../../database/dbConnection";
 import jwt from "jsonwebtoken";
-
-import CpfValidator from "../utils/cpfValidator";
 import moment from "moment";
+
+import User from "../model/User";
+import Profile from "../model/Profile";
+import { Repository } from "typeorm";
+import AppDataSource from "../../../database/dbConnection";
+import CpfValidator from "../utils/cpfValidator";
 
 // const sendMailPromise = require('../../Emails/mailer/mailer');
 // const helper = require('../../Emails/controllers/candidatoHelper');
@@ -14,89 +14,113 @@ import moment from "moment";
 
 class UserController {
   async store(req: Request, res: Response): Promise<Response> {
+    let { cpf, name, email, profile, password, phone } = req.body;
+
+    let errors: String[] = [];
+
+    const profileRepository: Repository<Profile> =
+      AppDataSource.getRepository(Profile);
+
+    const userRepository: Repository<User> = AppDataSource.getRepository(User);
+
     try {
-      const { password, cpf, name, email, phone } = req.body;
+      let user: User = await userRepository.findOne({
+        where: { cpf: cpf },
+      });
 
-      let errors: String[] = [];
+      if (user) {
+        return res.status(400).json({
+          errors: `Um usuário com o CPF: ${cpf} já está registrado. Verifique se o usuário está correto.`,
+        });
+      }
 
-      // emailValidation
+      if (!CpfValidator.validate(cpf)) {
+        errors.push("O CPF inserido é inválido.");
+      }
+
+      if (!name) {
+        errors.push("Um nome é obrigatório.");
+      }
+
       if (!email) {
-        errors.push("Coloque um email");
+        errors.push("Um e-mail é obrigatório.");
       }
 
-      // Validação CPF
+      if (!password) {
+        errors.push("Uma senha é obrigatória.");
+      }
 
-      // Validação de telefone
+      if (profile) {
+        try {
+          profile = await profileRepository.findOne({
+            where: { name: profile },
+          });
+        } catch (error: any) {
+          errors.push("O perfil inserido é inválido.");
+        }
+      }
+
+      if (!profile) {
+        profile = await profileRepository.findOne({
+          where: { name: "default_user" },
+        });
+
+        if (!profile) {
+          profile = profileRepository.create({
+            name: "default_user",
+            description: "Permissão de usuário padrão do sistema.",
+            isAdmin: false,
+          });
+          await profileRepository.save(profile);
+        }
+      }
+
       if (!phone) {
-        errors.push("Coloque um telefone");
+        errors.push("Um número de telefone é obrigatório.");
       }
 
-      // Verificação de erros
       if (errors.length > 0) {
         return res.status(400).json({ errors });
       }
 
-      const profileRepository = AppDataSource.getRepository(Profile);
-      const userRepository = AppDataSource.getRepository(User);
-
-      // Criação do perfil, se necessário
-      let profile = await profileRepository.findOne({
-        where: { name: "User_comun" },
+      user = userRepository.create({
+        cpf,
+        name,
+        email,
+        profile,
+        phone,
+        isAtivo: true,
       });
 
-      if (!profile) {
-        profile = profileRepository.create({
-          name: "User_comun",
-          description: "usuario comun",
-          isAdmin: false,
-        });
-        await profileRepository.save(profile);
-      }
+      user.password = password;
 
-      try {
-        // Criação do usuário
-        const newUser = userRepository.create({
-          profile,
-          password,
-          cpf,
-          name,
-          email,
-          phone,
-          isAtivo: true,
-        });
+      await userRepository.save(user);
 
-        newUser.password = password;
-
-        await userRepository.save(newUser);
-
-        const token = jwt.sign(
-          {
-            id: newUser.id,
-            name: newUser.name,
-            email: newUser.email,
-            profile_id: newUser.profile.id,
-          },
-          process.env.TOKEN_SECRET as string,
-          {
-            expiresIn: process.env.TOKEN_EXPIRATION,
-          }
-        );
-
-        return res.json({
-          token,
-          user: { name: newUser.name, email: newUser.email },
-        });
-      } catch (err: any) {
-        if (err.name === "QueryFailedError" && err.code === "23505") {
-          const errorMessages = (err.detail || "Erro de unicidade").split("\n");
-          return res.status(400).json({ errors: errorMessages });
+      const token = jwt.sign(
+        {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          profile_id: user.profile.id,
+        },
+        process.env.TOKEN_SECRET as string,
+        {
+          expiresIn: process.env.TOKEN_EXPIRATION,
         }
-        console.error(err);
-        return res.status(500).json({ error: "Erro interno do servidor" });
-      }
+      );
+
+      return res.json({
+        token,
+        user: { name: user.name, email: user.email },
+      });
     } catch (err: any) {
+      if (err.name === "QueryFailedError" && err.code === "23505") {
+        const errorMessages = (err.detail || "Erro de unicidade").split("\n");
+        return res.status(400).json({ errors: errorMessages });
+      }
+
       console.error(err.message);
-      return res.status(500).json({ error: err.message });
+      return res.status(500).json({ error: "Erro interno do servidor" });
     }
   }
 }
