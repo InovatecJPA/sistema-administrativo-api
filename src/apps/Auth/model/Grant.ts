026b47c3576e4741a3509eb3a5b49725a7c17642
ArthurRamonSouza <arthuramon.souza93@hotmail.com>
2024-08-26 16:27:48 -0300

    refactor(Grant): TypeOrm não aceita atributos privados

--- src/apps/Auth/model/Grant.ts
+++ src/apps/Auth/model/Grant.ts
@@ -9,99 +9,41 @@ import {
 @Entity("grants")
 class Grant {
   @PrimaryGeneratedColumn("uuid")
-  private id: string;
+  public id: string;
 
   @Column({ type: "varchar", unique: true, nullable: false })
-  private grant: string;
+  public name: string;
 
   @Column({ type: "text", nullable: true })
-  private note: string;
+  public note: string;
 
-  @Column({ type: "varchar", nullable: true })
-  private routeFilter: string;
+  @Column({ type: "varchar", nullable: true, name: "route_filter" })
+  public routeFilter: string;
 
   @Column({ type: "varchar", nullable: false })
-  private route: string;
+  public route: string;
 
-  @CreateDateColumn({ type: "timestamp with time zone", nullable: false })
-  private created_at: Date;
+  @CreateDateColumn({ type: "timestamp with time zone", nullable: false, name: "created_at" })
+  public createdAt: Date;
 
-  @UpdateDateColumn({ type: "timestamp with time zone", nullable: false })
-  private updated_at: Date;
+  @UpdateDateColumn({ type: "timestamp with time zone", nullable: false, name: "updated_at" })
+  public updatedAt: Date;
 
   // All-args constructor
   constructor(
-    grant: string,
+    name: string,
     note: string,
     routeFilter: string,
     route: string,
-    created_at: Date,
-    updated_at: Date
+    createdAt: Date,
+    updatedAt: Date
   ) {
-    this.grant = grant;
+    this.name = name;
     this.note = note;
     this.routeFilter = routeFilter;
     this.route = route;
-    this.created_at = created_at;
-    this.updated_at = updated_at;
-  }
-
-  // Getters and Setters
-
-  public getId(): string {
-    return this.id;
-  }
-
-  public setId(id: string): void {
-    this.id = id;
-  }
-
-  public getGrant(): string {
-    return this.grant;
-  }
-
-  public setGrant(grant: string): void {
-    this.grant = grant;
-  }
-
-  public getNote(): string {
-    return this.note;
-  }
-
-  public setNote(note: string): void {
-    this.note = note;
-  }
-
-  public getRouteFilter(): string {
-    return this.routeFilter;
-  }
-
-  public setRouteFilter(routeFilter: string): void {
-    this.routeFilter = routeFilter;
-  }
-
-  public getRoute(): string {
-    return this.route;
-  }
-
-  public setRoute(route: string): void {
-    this.route = route;
-  }
-
-  public getCreatedAt(): Date {
-    return this.created_at;
-  }
-
-  public setCreatedAt(created_at: Date): void {
-    this.created_at = created_at;
-  }
-
-  public getUpdatedAt(): Date {
-    return this.updated_at;
-  }
-
-  public setUpdatedAt(updated_at: Date): void {
-    this.updated_at = updated_at;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
   }
 }
 
