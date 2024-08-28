6c8b65382cc331a8c0a6fe2a0cdda15a12dba05a
ArthurRamonSouza <arthuramon.souza93@hotmail.com>
2024-08-28 12:52:47 -0300

    refactor: GrantService implementa ServiceInterface e os métodos de GrantController foram alterados e renomeados para melhorar o cõdigo. Documentação foi adicionada.

--- src/apps/Auth/controller/GrantController.ts
+++ src/apps/Auth/controller/GrantController.ts
@@ -6,88 +6,166 @@ import CustomValidationError from "../error/CustomValidationError";
 import { DeleteResult } from "typeorm";
 import { Request, Response, NextFunction } from "express";
 
+/**
+ * Controller for handling requests related to `Grant` resources.
+ */
 export default class GrantController {
     private grantService: GrantService;
 
+    /**
+     * Initializes a new instance of the `GrantController` class.
+     * 
+     * @param grantService - The service instance to handle `Grant` business logic.
+     */
     constructor(grantService: GrantService) {
         this.grantService = grantService;
     }
 
-    async create(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
-        try{
+    /**
+     * Handles POST requests to create a new `Grant`.
+     * 
+     * @param req - The HTTP request object.
+     * @param res - The HTTP response object.
+     * @param next - The next middleware function in the stack.
+     * 
+     * @returns The created `Grant` entity or calls the next middleware with an error.
+     */
+    async post(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+        try {
             const { name, note, routeFilter, route } = req.body;
             const grantDto = new GrantDto(name, note, routeFilter, route);
-            
+
             if (!grantDto.isValid())
-                throw new CustomValidationError("Todos os campos da nova permissão devem ser diferentes de nulo ou \"\".", 400);
-            
+                throw new CustomValidationError("All fields of the new permission must be non-null or \"\".", 400);
+
             const newGrant: Grant = await this.grantService.save(grantDto);
 
             return res.status(201).json(newGrant);
 
-        } catch(error: any) {
+        } catch (error: any) {
             next(error);
         }
     }
 
+    /**
+     * Handles GET requests to retrieve a `Grant` by its name.
+     * 
+     * @param req - The HTTP request object.
+     * @param res - The HTTP response object.
+     * @param next - The next middleware function in the stack.
+     * 
+     * @returns The `Grant` entity with the specified name or calls the next middleware with an error.
+     */
     async get(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
         try {
             const { name } = req.params;
-            const grant: Grant | null = await this.grantService.findOneByName(name);
+            const grant: Grant | null = await this.grantService.findOne({ name });
 
             if (!grant)
-                throw new CustomValidationError(`Permissão ${name} não encontrada.`, 400);
+                throw new CustomValidationError(`Permission ${name} not found.`, 400);
 
             return res.status(200).json(grant);
 
-        } catch(error: any) {
+        } catch (error: any) {
             next(error);
-        }        
+        }
     }
-    
+
+    /**
+     * Handles GET requests to retrieve a `Grant` by its ID.
+     * 
+     * @param req - The HTTP request object.
+     * @param res - The HTTP response object.
+     * @param next - The next middleware function in the stack.
+     * 
+     * @returns The `Grant` entity with the specified ID or calls the next middleware with an error.
+     */
+    async getById(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+        try {
+            const { id } = req.params;
+            const grant: Grant | null = await this.grantService.findOneById(id);
+
+            if (!grant)
+                throw new CustomValidationError(`Permission with ID ${id} not found.`, 400);
+
+            return res.status(200).json(grant);
+
+        } catch (error: any) {
+            next(error);
+        }
+    }
+
+    /**
+     * Handles GET requests to retrieve all `Grant` entities.
+     * 
+     * @param req - The HTTP request object.
+     * @param res - The HTTP response object.
+     * @param next - The next middleware function in the stack.
+     * 
+     * @returns A list of all `Grant` entities or calls the next middleware with an error.
+     */
     async getAll(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
         try {
             const grants: Grant[] | null = await this.grantService.findAll();
 
             if (!grants)
-                throw new CustomValidationError("Nenhuma permissão encontrada.", 400);
+                throw new CustomValidationError("No permissions found.", 400);
 
             return res.status(200).json(grants);
 
-        } catch(error: any) {
+        } catch (error: any) {
             next(error);
         }
     }
 
-    async update(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+    /**
+     * Handles PUT requests to update a `Grant`.
+     * 
+     * @param req - The HTTP request object.
+     * @param res - The HTTP response object.
+     * @param next - The next middleware function in the stack.
+     * 
+     * @returns The updated `Grant` entity or calls the next middleware with an error.
+     */
+    async put(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
         try {
             const { id } = req.params;
             const { name, note, routeFilter, route } = req.body;
             const grantDto = new GrantDto(name, note, routeFilter, route);
-            
+
             if (!grantDto.isValid())
-                throw new CustomValidationError("Todos os campos da permissão devem ser diferentes de nulo ou \"\".", 400);
-            
+                throw new CustomValidationError("All fields of the permission must be non-null or \"\".", 400);
+
             let updatedGrant: Grant = grantDto.toGrant();
             updatedGrant.id = id;
-            
+
             updatedGrant = await this.grantService.save(grantDto);
             return res.status(200).json(updatedGrant);
 
-        } catch(error: any) {
+        } catch (error: any) {
             next(error);
         }
     }
-    
+
+    /**
+     * Handles DELETE requests to remove a `Grant` by its ID.
+     * 
+     * @param req - The HTTP request object.
+     * @param res - The HTTP response object.
+     * @param next - The next middleware function in the stack.
+     * 
+     * @returns Status 200 if the deletion was successful or calls the next middleware with an error.
+     */
     async delete(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
         try {
             const { id } = req.params;
-            const deleted:DeleteResult = await this.grantService.delete(id);
+            const deleted: DeleteResult = await this.grantService.delete(id);
+
             if (deleted.affected === 0)
-                throw new CustomValidationError(`Permissão com id ${id} não encontrada.`, 400);
+                throw new CustomValidationError(`Permission with ID ${id} not found.`, 400);
 
             return res.status(200).send();
-        } catch(error: any) {
+        } catch (error: any) {
             next(error);
         }
     }
--- src/apps/Auth/service/GrantService.ts
+++ src/apps/Auth/service/GrantService.ts
@@ -1,45 +1,111 @@
 import Grant from "../model/Grant";
 import GrantDto from "../dto/GrantDto";
+import ServiceInterface from "../interface/ServiceInterface";
 import CustomValidationError from "../error/CustomValidationError";
 
 import { DeleteResult, Repository } from "typeorm";
 
-export default class GrantService {
+/**
+ * Service class for managing `Grant` entities.
+ */
+export default class GrantService implements ServiceInterface<Grant, GrantDto> {
 
     private grantRepository: Repository<Grant>;
 
-    constructor(grantRepository: Repository<Grant>) {
-        this.grantRepository = grantRepository;
+    /**
+     * Initializes a new instance of the `GrantService` class.
+     * 
+     * @param repository - The TypeORM repository for `Grant` entities.
+     */
+    constructor(repository: Repository<Grant>) {
+        this.grantRepository = repository;
     }
 
-    async save(grantDto: GrantDto): Promise<Grant> {
+    /**
+     * Saves a new `Grant` entity to the database.
+     * 
+     * @param grantDto - The data transfer object containing the `Grant` details.
+     * 
+     * @returns The saved `Grant` entity.
+     */
+    async save(grantDto: Partial<GrantDto>): Promise<Grant> {
         const now: Date = new Date();
-        const newGrant: Grant = new Grant(grantDto.getName(), grantDto.getNote(), grantDto.getRoute(), grantDto.getRouteFilter(), now, now);
+        const newGrant: Grant = new Grant(
+            grantDto.getName(), 
+            grantDto.getNote(), 
+            grantDto.getRoute(), 
+            grantDto.getRouteFilter(), 
+            now, 
+            now
+        );
         
         return this.grantRepository.save(newGrant);
     }
 
-    async findOneByName(name: string): Promise<Grant | null> {
+    /**
+     * Finds a `Grant` entity by partial matching of its properties.
+     * 
+     * @param object - Partial `Grant` object containing properties to match.
+     * 
+     * @returns The matched `Grant` entity or null if not found.
+     */
+    async findOne(object: Partial<Grant>): Promise<Grant | null> {
         return await this.grantRepository.findOne({
-            where: { name: name },
-        });        
+            where: { ...object },
+        });  
     }
 
+    /**
+     * Finds a `Grant` entity by its ID.
+     * 
+     * @param id - The ID of the `Grant` entity to find.
+     * 
+     * @returns The `Grant` entity with the specified ID or null if not found.
+     */
     async findOneById(id: string): Promise<Grant | null> {
         return await this.grantRepository.findOne({
             where: { id: id },
         });        
     }
-    
+
+    /**
+     * Retrieves all `Grant` entities from the database.
+     * 
+     * @returns An array of all `Grant` entities or null if none found.
+     */
     async findAll(): Promise<Grant[] | null> {
         return await this.grantRepository.find();
     }
+
+    /**
+     * Updates an existing `Grant` entity.
+     * 
+     * @param id - The ID of the `Grant` entity to update.
+     * @param object - Partial `Grant` object containing updated properties.
+     * 
+     * @returns The updated `Grant` entity.
+     */
+    async update(id: string, object: Partial<Grant>): Promise<Grant> {
+        return this.grantRepository.save({
+            ...object,
+            id: id
+        });
+    }
     
+    /**
+     * Deletes a `Grant` entity by its name.
+     * 
+     * @param name - The name of the `Grant` entity to delete.
+     * 
+     * @returns The result of the delete operation.
+     * 
+     * @throws CustomValidationError if no `Grant` with the specified name is found.
+     */
     async delete(name: string): Promise<DeleteResult> {
         const grantToDelete = await this.grantRepository.findOneBy({ name });
         
         if (!grantToDelete) {
-            throw new CustomValidationError(`Permissão com nome ${name} não encontrada.`, 400);
+            throw new CustomValidationError(`Permission with name ${name} not found.`, 400);
         }
 
         return await this.grantRepository.delete({ name });
