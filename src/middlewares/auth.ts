a6126fdd7fdc77c1e3fd1a1a6ee7d92977b9e929
eduardoramalhoz <131375484+eduardoramalhoz@users.noreply.github.com>
2024-08-29 15:25:20 -0300

    feat(auth): adicionando documentação

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,69 +1,81 @@
 import * as jwt from "../config/jwt";
-import Grant from "../apps/Auth/model/Grant";
 import ProfileGrant from "../apps/Auth/model/ProfileGrant";
 import * as UserDTO from "../apps/Auth/interface/userInterfaces";
 import { NextFunction, Request, Response } from "express";
 import AppDataSource from "../database/dbConnection";
 
+/**
+ * Checks if the user has permission to access a specific route based on their profile grants.
+ * @param userInfo - The user information including the requested path.
+ * @param profileGrants - The list of profile grants associated with the user's profile.
+ * @returns `true` if the user has permission to access the route, otherwise `false`.
+ */
 const checkRoutePermission = (
   userInfo: UserDTO.userInfo,
   profileGrants: ProfileGrant[]
-) => {
-  console.log(userInfo.path)
-  // Verifica se o caminho atual é permitido por padrão
+): boolean => {
+  console.log(userInfo.path);
+
+  // Define default allowed paths that all users can access
   const defaultAllowedPaths = ["/dev/accounts/users/profile", `/users/${userInfo.id}`];
 
+  // If the user's path is in the default allowed paths, grant access
   if (defaultAllowedPaths.includes(userInfo.path)) return true;
 
-  // Expressões regulares para verificar permissões baseadas em padrões de caminho
+  // Define regex patterns for routes that are allowed by default
   const regexPatterns = [
     /\/[a-zA-Z]+\/upload\//,
     /^\/upload\/get\//,
     /\/[a-zA-Z]+\/cpf\//,
   ];
 
-  // Verifica se o caminho do usuário corresponde a qualquer um dos padrões regex
+  // Check if the user's path matches any of the regex patterns
   if (regexPatterns.some((regex) => regex.test(userInfo.path))) return true;
 
-  // Regex para UUIDs em caminhos
+  // Regex pattern for UUIDs in routes
   const uuidRegex =
     /(\{)?[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}(\})?/;
   const uuidMatches = userInfo.path.match(uuidRegex);
   const uuid = uuidMatches ? uuidMatches[0] : null;
 
-  // Cria uma lista de rotas permitidas, substituindo :id pelo UUID se aplicável
+  // Generate a list of routes with UUIDs replaced if applicable
   const grants = profileGrants.map((profileGrant) => {
     const { route } = profileGrant.grant;
     return route.includes(":id") && uuid ? route.replace(":id", uuid) : route;
   });
 
-  // Verifica se o caminho do usuário está nas rotas permitidas
+  // Check if the user's path is in the list of permitted routes
   return grants.includes(userInfo.path);
 };
 
+/**
+ * Filters and sets the route filter based on the profile grants.
+ * @param userInfo - The user information including the requested path.
+ * @param profileGrants - The list of profile grants associated with the user's profile.
+ */
 const filterRoute = (
   userInfo: UserDTO.userInfo,
   profileGrants: ProfileGrant[]
-) => {
-  // Expressão regular para encontrar um UUID na rota do usuário
+): void => {
+  // Regex pattern for UUIDs in routes
   const uuidRegex =
     /(\{)?[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}(\})?/;
   const matches = userInfo.path.match(uuidRegex);
   const uuid = matches ? matches[0] : null;
 
-  // Mapeia as rotas de concessão, substituindo :id pelo UUID se aplicável
+  // Map grants to routes, replacing UUID if applicable
   const grants = profileGrants.map((profileGrant) => {
     const { route } = profileGrant.grant;
     return route.includes(":id") && uuid ? route.replace(":id", uuid) : route;
   });
 
-  // Encontra o índice da rota do usuário na lista de rotas de concessão
+  // Find the index of the user's path in the list of permitted routes
   const index = grants.indexOf(userInfo.path);
 
-  // Se a rota do usuário não for encontrada, não faz nenhuma modificação
+  // If the route is not found, no filtering is done
   if (index === -1) return;
 
-  // Mapeia todas as rotas filtráveis e define a rota filtrável correspondente no userInfo
+  // Map and set the route filter based on the index
   const filterableRoutes = profileGrants.map(
     (profileGrant) => profileGrant.grant.routeFilter
   );
@@ -71,58 +83,64 @@ const filterRoute = (
   userInfo.routeFilter = filterableRoutes[index];
 };
 
+/**
+ * Middleware function to handle authentication and authorization.
+ * @param req - The request object.
+ * @param res - The response object.
+ * @param next - The next middleware function.
+ */
 export const authMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  // Retrieve the authorization header from the request
   const authHeader = req.headers.authorization;
 
+  // Check if the authorization header is present
   if (!authHeader) {
-    return res.status(401).json({ error: "Token not provided" });
+    return res.status(401).json({ error: "Token not provided." });
   }
 
+  // Split the authorization header into scheme and token
   const parts = authHeader.split(" ");
 
+  // Validate the authorization header format
   if (parts.length !== 2) {
-    return res.status(401).json({ error: "Token error" });
+    return res.status(401).json({ error: "Token error." });
   }
 
   const [scheme, token] = parts;
 
+  // Check if the authorization scheme is 'Bearer'
   if (!/^Bearer$/i.test(scheme)) {
-    return res.status(401).json({ error: "Token malformatted" });
+    return res.status(401).json({ error: "Token poorly formatted." });
   }
 
+  // Verify the JWT token and extract user information
   const userInfo = jwt.verifyToken(token);
 
+  // If the token is invalid, return an error response
   if (!userInfo) {
-    return res.status(401).json({ error: "Token invalid" });
+    return res.status(401).json({ error: "Token invalid." });
   }
 
-  //console.log(req.query);
-
+  // Assign the user information to the request object
   req.userInfo = userInfo as UserDTO.userInfo;
 
-  //console.log(userInfo);
-
-  // Questão de grants e etc
+  // Extract the base path from the request URL (excluding query parameters)
   const startParams = req.originalUrl.indexOf("?");
-  console.log(`\n StartParams: ${startParams} \n`);
-
   req.userInfo.path = req.originalUrl.substring(
     0,
     startParams === -1 ? req.originalUrl.length : startParams
   );
 
-  //console.log(req.userInfo.path);
-
-  // Era pra ser uma função do service de grant
+  // Retrieve profile grants from the database
   const profileGrantRepository = AppDataSource.getRepository(ProfileGrant);
 
   const profileGrants = await profileGrantRepository.find({
-    where: { profile: { id: userInfo.profileId } }, // Filtra os ProfileGrant associados ao Profile especificado
-    relations: ["grant"], // Inclui os dados da entidade Grant
+    where: { profile: { id: userInfo.profileId } }, // Filter grants by the user's profile ID
+    relations: ["grant"], // Include the related Grant entity
     select: {
       grant: {
         route: true,
@@ -131,15 +149,17 @@ export const authMiddleware = async (
     },
   });
 
+  // Check if the user has permission to access the route
   if (!userInfo.isAdmin && !checkRoutePermission(req.userInfo, profileGrants)) {
     return res.status(403).send();
   } else {
     try {
+      // Filter and set the route filter if applicable
       filterRoute(req.userInfo, profileGrants);
-      return next();
+      return next(); // Proceed to the next middleware or route handler
     } catch (error) {
-      console.error(error);
-      res.status(400).send();
+      console.error(error); // Log any errors
+      res.status(400).send(); // Return a bad request response on error
     }
   }
 };
