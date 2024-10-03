a357d184a8f32f3661f90a7fb3eead5105adce7a
João Roberto <robertoo.jof@gmail.com>
2024-10-03 12:00:48 -0300

    fix(errorsHandler):
    Ajusta middleware de tratamento de erros e printa IP atual

--- src/middlewares/errorsHandler.ts
+++ src/middlewares/errorsHandler.ts
@@ -7,27 +7,28 @@ export const errorsHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof SyntaxError && "body" in err) {
-    console.log("Invalid JSON");
-    console.log(req);
-    console.log("req.body", req.body);
-
+  // Erro de sintaxe no JSON
+  if (
+    err instanceof SyntaxError &&
+    (err as any).status === 400 &&
+    "body" in err
+  ) {
+    console.error("Invalid JSON syntax error detected.");
     return res
       .status(400)
       .json({ status: 400, message: "Invalid JSON format." });
   }
 
+  // Erros personalizados
   if (err instanceof BaseError) {
-    console.log("Base error: ", err.message);
-    console.log(req);
-    console.log("req.body", req.body);
-
+    console.error("Base error: ", err.message);
     return res.status(err.status).json({
       error: err.message,
     });
   }
 
-  console.log("Unexpected server error: ", err);
+  // Erro genérico
+  console.error("Unexpected server error: ", err);
   res.status(500).json({
     status: 500,
     error: "Internal server error.",
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import app from "./app";
+import os from "os";
 
 const envFile =
 	process.env.NODE_ENV === "development" ? ".env.development" : ".env.deploy";
@@ -7,6 +8,20 @@ dotenv.config({ path: envFile });
 
 const port = process.env.APP_PORT;
 app.listen(port);
+
+const getLocalIP = (): string => {
+  const interfaces = os.networkInterfaces();
+  for (const iface in interfaces) {
+    for (const alias of interfaces[iface]!) {
+      if (alias.family === "IPv4" && !alias.internal) {
+        return alias.address; // Retorna o primeiro endereço IP encontrado
+      }
+    }
+  }
+  return "127.0.0.1"; // Retorno padrão se não encontrar um IP válido
+};
+
+console.log("IP da rede local:", getLocalIP());
 
 console.log(`Running in ${process.env.NODE_ENV} mode`);
 console.log(`Server running on port ${port}`);
