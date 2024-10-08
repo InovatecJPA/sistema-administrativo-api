a8bf9fb08dea63b27899ab1a13ecdcd490f769af
Jose Vitor <josevitorrodrigues17@gmail.com>
2024-10-08 14:35:13 -0300

    feat:services for chat

---
+++ src/apps/Messaging/service/ChatService.ts
@@ -0,0 +1,51 @@
+import { DeleteResult, FindOptionsWhere, Repository } from "typeorm";
+import Chat from "../model/Chat";
+import { InvalidObjectError } from "../../../error/InvalidObjectError";
+import AppDataSource from "../../../database/dbConnection";
+
+export class ChatService {
+
+  private chatRepository: Repository<Chat>;
+
+  constructor(chatRepository: Repository<Chat>) {
+    this.chatRepository = chatRepository;
+  }
+
+  async save(chat: Partial<Chat>): Promise<Chat> {
+    
+    if (chat.users && chat.users.length > 0) {
+      return this.chatRepository.save(chat);
+    } else {
+      throw new InvalidObjectError("Invalid chat object. Chat must have at least one user.");
+    }
+  }
+
+  async findOneById(id: string): Promise<Chat> {
+    return this.chatRepository.findOneBy({ id });
+  }
+
+  async findAll(): Promise<Chat[]> {
+    return this.chatRepository.find({ 
+      relations: ["users", "messages"], 
+    });
+  }
+
+  async update(id: string, chat: Partial<Chat>): Promise<Chat> {
+    return this.chatRepository.save({
+      ...chat,
+      id,
+    });
+  }
+
+  async delete(id: string): Promise<DeleteResult> {
+    const chatToDelete = await this.chatRepository.findOneBy({ id });
+    if (!chatToDelete) {
+      throw new InvalidObjectError(`Chat with ID ${id} not found`);
+    }
+    return await this.chatRepository.delete({ id });
+  }
+}
+
+
+const chatRepository: Repository<Chat> = AppDataSource.getRepository(Chat);
+export const chatService = new ChatService(chatRepository);
