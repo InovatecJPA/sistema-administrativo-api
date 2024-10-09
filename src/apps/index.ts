e4f9b3eaebedad451a2c190be7158d72b232f7f1
ArthurRamonSouza <arthuramon.souza93@hotmail.com>
2024-10-09 16:06:48 -0300

    test: testoing the auth middleware

--- src/apps/index.ts
+++ src/apps/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import testRouter from "../teste";
 import projectRequestRouter from "./Api/routes/ProjectRequestRoutes";
 import projectRouter from "./Api/routes/ProjectRoutes";
 import sectorRouter from "./Api/routes/SectorRoutes";
 import authenticationRouter from "./Auth/routes";
-import messagesRouter from "./Messaging/routes/MessageRoutes";
 import chatRouter from "./Messaging/routes/ChatRoutes";
+import messagesRouter from "./Messaging/routes/MessageRoutes";
 
 const router: Router = Router();
 
@@ -14,5 +15,6 @@ router.use("/projects", projectRouter);
 router.use("/projectRequests", projectRequestRouter);
 router.use("/sectors", sectorRouter);
 router.use("/chat", chatRouter);
+router.use("/test", testRouter);
 
 export default router;
