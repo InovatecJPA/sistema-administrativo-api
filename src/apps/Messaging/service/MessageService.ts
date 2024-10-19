759db13a08272df96c93fd054a82d1fddd23e7a8
Jose <josevitorrodrigues17@gmail.com>
2024-10-19 09:36:16 -0300

    feat: add chat creation where some user send message

--- src/apps/Messaging/service/MessageService.ts
+++ src/apps/Messaging/service/MessageService.ts
@@ -1,29 +1,85 @@
-import { DeleteResult, FindOptionsWhere, LessThan, MoreThanOrEqual, Repository } from "typeorm";
+import { 
+  DeleteResult, 
+  FindOptionsWhere, 
+  LessThan, 
+  MoreThanOrEqual, 
+  Repository 
+} from "typeorm";
 import ServiceInterface from "../../Auth/interface/ServiceInterface";
 import MessageDto from "../dto/MessageDto";
 import Message from "../model/Message";
 import { InvalidObjectError } from "../../../error/InvalidObjectError";
 import AppDataSource from "../../../database/dbConnection";
+import { chatService } from "./ChatService";
+import User from "../../Auth/model/User";
+import Chat from "../model/Chat";
 
-export class MessageService implements ServiceInterface<Message, MessageDto> {
-
+export class MessageService {
   private messageRepository: Repository<Message>;
+  private userRepository: Repository<User>;
+  private chatRepository: Repository<Chat>;
 
-  constructor(messageRepository: Repository<Message>) {
+  constructor(
+    messageRepository: Repository<Message>,
+    userRepository: Repository<User>,
+    chatRepository: Repository<Chat>
+  ) {
     this.messageRepository = messageRepository;
+    this.userRepository = userRepository;
+    this.chatRepository = chatRepository;
   }
 
-
-  async save(objectDto: Partial<MessageDto>): Promise<Message> {
-    if (objectDto.isValid()) {
-      return this.messageRepository.save(objectDto.toMessage());
-    } else {
+  async sendMessage(messageDto: MessageDto): Promise<Message> {
+    if (!messageDto.isValid()) {
       throw new InvalidObjectError("Invalid message object.");
     }
+
+    const senderId = messageDto.getSenderId();
+    const receiverId = messageDto.getReceiverId();
+    const text = messageDto.getText();
+
+   
+    const [sender, receiver] = await Promise.all([
+      this.userRepository.findOne({ where: { id: senderId } }),
+      this.userRepository.findOne({ where: { id: receiverId } }),
+    ]);
+
+    if (!sender || !receiver) {
+      throw new Error("Sender or receiver not found.");
+    }
+
+    let chat = await this.chatRepository
+      .createQueryBuilder("chat")
+      .innerJoin("chat.users", "user")
+      .where("user.id IN (:...userIds)", { userIds: [senderId, receiverId] })
+      .groupBy("chat.id")
+      .having("COUNT(user.id) = 2")
+      .getOne();
+
+    
+    if (!chat) {
+      chat = this.chatRepository.create({
+        name: `${sender.name} & ${receiver.name}`,
+        users: [sender, receiver],
+      });
+      await this.chatRepository.save(chat);
+    }
+
+   
+    const message = this.messageRepository.create({
+      text,
+      sender,
+      receiver,
+      chat,
+      sendedAt: new Date(),
+    });
+
+    
+    return await this.messageRepository.save(message);
   }
 
   async findOne(object: Partial<Message>): Promise<Message> {
-    return this.messageRepository.findOne({ 
+    return this.messageRepository.findOne({
       where: object as FindOptionsWhere<Message>,
     });
   }
@@ -38,35 +94,42 @@ export class MessageService implements ServiceInterface<Message, MessageDto> {
 
   async findAllByDate(sendedAtDay: string): Promise<Message[]> {
     const startOfDay = new Date(sendedAtDay);
-        startOfDay.setHours(0, 0, 0, 0);
-        
-        const endOfDay = new Date(sendedAtDay);
-        endOfDay.setHours(23, 59, 59, 999);
+    startOfDay.setHours(0, 0, 0, 0);
 
-        return this.messageRepository.find({
-            where: [
-                { sendedAt: MoreThanOrEqual(startOfDay) },
-                { sendedAt: LessThan(endOfDay)}
-            ]
-            });
+    const endOfDay = new Date(sendedAtDay);
+    endOfDay.setHours(23, 59, 59, 999);
+
+    return this.messageRepository.find({
+      where: [
+        { sendedAt: MoreThanOrEqual(startOfDay) },
+        { sendedAt: LessThan(endOfDay) },
+      ],
+    });
   }
 
   async update(id: string, object: Partial<Message>): Promise<Message> {
     return this.messageRepository.save({
       ...object,
       id,
-    })
+    });
   }
-  
+
   async delete(id: string): Promise<DeleteResult> {
-    const profileToDelete = await this.messageRepository.findOneBy({ id });
-    if (!profileToDelete) {
+    const messageToDelete = await this.messageRepository.findOneBy({ id });
+    if (!messageToDelete) {
       throw new InvalidObjectError(`Message with ID ${id} not found`);
     }
     return await this.messageRepository.delete({ id });
   }
 }
 
-// Initialize the repository and export the service instance
+// Inicializa os repositórios e exporta a instância do serviço
 const messageRepository: Repository<Message> = AppDataSource.getRepository(Message);
-export const messageService = new MessageService(messageRepository);
+const userRepository: Repository<User> = AppDataSource.getRepository(User);
+const chatRepository: Repository<Chat> = AppDataSource.getRepository(Chat);
+
+export const messageService = new MessageService(
+  messageRepository,
+  userRepository,
+  chatRepository
+);
