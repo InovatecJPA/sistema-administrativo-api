5aa26a3c61280bec04e82d1d83a7ea03248d7bb5
Roberto <robertoo.jof@gmail.com>
2024-11-20 01:06:17 -0300

    doc(profile):
    Mapeia e documenta todo o fluxo de resposta e requisição de profile

--- src/apps/Auth/controller/ProfileController.ts
+++ src/apps/Auth/controller/ProfileController.ts
@@ -28,15 +28,17 @@ export class ProfileController {
    */
   async createProfiles(res: Response, next: NextFunction): Promise<Response> {
     const profiles = {
-      admin: 'Administrador do sistema',
-      administrativo: 'Setor administrativo da empresa',
+      admin: "Administrador do sistema",
+      administrativo: "Setor administrativo da empresa",
     };
 
     try {
       for (const [key, value] of Object.entries(profiles)) {
         await this.profileService.save(new ProfileDto(key, value));
       }
-      return res.status(200).json({ message: 'All profiles seeded successfully' });
+      return res
+        .status(200)
+        .json({ message: "All profiles seeded successfully" });
     } catch (error) {
       next(error);
     }
@@ -50,11 +52,13 @@ export class ProfileController {
    * @param next - The next middleware function.
    * @returns A JSON response indicating success or passes errors to the next middleware.
    */
-  public store = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  public store = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
     try {
-      await Promise.all([
-        this.createProfiles(res, next),
-      ]);
+      await Promise.all([this.createProfiles(res, next)]);
     } catch (error) {
       next(error);
     }
@@ -67,10 +71,14 @@ export class ProfileController {
    * @param res - The HTTP response object used to send the newly created `Profile` entity in JSON format.
    * @param next - The next middleware function in the stack, used for error handling.
    *
-   * @returns A `Response` object with status 201 and the newly created `Profile` entity in JSON format, 
+   * @returns A `Response` object with status 201 and the newly created `Profile` entity in JSON format,
    * or calls the `next` middleware with an error if validation fails or an error occurs.
    */
-  public post = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+  public post = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> => {
     try {
       const { name, description, users, associatedGrants } = req.body;
 
@@ -81,7 +89,10 @@ export class ProfileController {
         if (!userExists) {
           throw new CustomValidationError(`User with ID ${user} not found.`);
         }
-        this.profileService.addUserToProfile(userExists, profileDto.toProfile());
+        this.profileService.addUserToProfile(
+          userExists,
+          profileDto.toProfile()
+        );
       });
 
       associatedGrants.forEach(async (grant: string) => {
@@ -89,45 +100,56 @@ export class ProfileController {
         if (!grantExists) {
           throw new CustomValidationError(`Grant with ID ${grant} not found.`);
         }
-        this.profileService.addGrantToProfile(grantExists, profileDto.toProfile());
+        this.profileService.addGrantToProfile(
+          grantExists,
+          profileDto.toProfile()
+        );
       });
 
       if (!profileDto.isValid())
-        throw new CustomValidationError('All fields of the new profile must be non-null or different of "" .');
+        throw new CustomValidationError(
+          'All fields of the new profile must be non-null or different of "" .'
+        );
 
       const newProfile: Profile = await this.profileService.save(profileDto);
 
       return res.status(201).json(newProfile);
-
     } catch (error: any) {
       next(error);
     }
-  }
+  };
 
   /**
-  * Handles GET requests to retrieve a `Profile` entity by its name.
-  *
-  * @param req - The HTTP request object, which should contain the `route` parameter in its route parameters.
-  * @param res - The HTTP response object used to send the response back to the client.
-  * @param next - The next middleware function in the Express.js stack, used to handle errors.
-  *
-  * @returns A `Response` object with status 200 and the `Profile` entity in JSON format if found, 
-  * or calls the `next` middleware with an error if the `Profile` is not found or an error occurs.
- */
-  public get = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+   * Handles GET requests to retrieve a `Profile` entity by its name.
+   *
+   * @param req - The HTTP request object, which should contain the `route` parameter in its route parameters.
+   * @param res - The HTTP response object used to send the response back to the client.
+   * @param next - The next middleware function in the Express.js stack, used to handle errors.
+   *
+   * @returns A `Response` object with status 200 and the `Profile` entity in JSON format if found,
+   * or calls the `next` middleware with an error if the `Profile` is not found or an error occurs.
+   */
+  public get = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> => {
     try {
       const { name } = req.params;
-      const profile: Profile | null = await this.profileService.findOne({ name });
+      const profile: Profile | null = await this.profileService.findOne({
+        name,
+      });
+
+      console.log(name);
 
       if (!profile)
         throw new CustomValidationError(`Profile ${name} not found.`);
 
       return res.status(200).json(profile);
-
     } catch (error: any) {
       next(error);
     }
-  }
+  };
 
   /**
    * Retrieve all profiles from the database.
@@ -137,14 +159,18 @@ export class ProfileController {
    * @param next - The next middleware function.
    * @returns A JSON response containing all profiles or passes errors to the next middleware.
    */
-  public getAll = async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
+  public getAll = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response> => {
     try {
       const profiles = await this.profileService.findAll();
       return res.json(profiles);
     } catch (error) {
       next(error);
     }
-  }
+  };
 
   /**
    * Retrieve a single profile by its ID.
@@ -154,7 +180,11 @@ export class ProfileController {
    * @param next - The next middleware function.
    * @returns A JSON response containing the profile details or passes errors to the next middleware.
    */
-  public getById = async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
+  public getById = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response> => {
     try {
       const profile = await this.profileService.findOneById(req.params.id);
 
@@ -166,7 +196,7 @@ export class ProfileController {
     } catch (error) {
       next(error);
     }
-  }
+  };
 
   /**
    * Handles PUT requests to update an existing `Profile`.
@@ -175,10 +205,14 @@ export class ProfileController {
    * @param res - The HTTP response object used to send the updated `Profile` entity in JSON format.
    * @param next - The next middleware function in the stack, used for error handling.
    *
-   * @returns A `Response` object with status 200 and the updated `Profile` entity in JSON format, 
+   * @returns A `Response` object with status 200 and the updated `Profile` entity in JSON format,
    * or calls the `next` middleware with an error if validation fails or an error occurs.
-  */
-  public put = async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
+   */
+  public put = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response> => {
     try {
       const profile = await this.profileService.findOneById(req.params.id);
 
@@ -195,7 +229,7 @@ export class ProfileController {
     } catch (error) {
       next(error);
     }
-  }
+  };
 
   /**
    * Delete a profile by its ID.
@@ -205,7 +239,11 @@ export class ProfileController {
    * @param next - The next middleware function.
    * @returns A JSON response indicating success or failure or passes errors to the next middleware.
    */
-  public deleteById = async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
+  public deleteById = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response> => {
     try {
       const result = await this.profileService.delete(req.params.id);
 
@@ -217,9 +255,13 @@ export class ProfileController {
     } catch (error) {
       next(error);
     }
-  }
+  };
 
-  public postUser = async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
+  public postUser = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response> => {
     const { profileId } = req.params;
     const { userId } = req.body;
 
@@ -228,34 +270,42 @@ export class ProfileController {
       if (!profile) {
         throw new NotFoundError(`Profile with ID ${profileId} not found.`);
       }
-  
+
       const user = await userService.findOne(userId);
       if (!user) {
         throw new NotFoundError(`User with ID ${userId} not found.`);
       }
-  
-      const profileUpdated: Profile = await this.profileService.addUserToProfile(user, profile);
-  
+
+      const profileUpdated: Profile =
+        await this.profileService.addUserToProfile(user, profile);
+
       return res.status(HttpStatusCode.Ok).json(profileUpdated);
     } catch (error) {
       next(error);
     }
-  }
+  };
 
-  public deleteUser = async (req: Request, res: Response, next: NextFunction): Promise<Response> => { 
+  public deleteUser = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response> => {
     const { profileId } = req.params;
     const { userId } = req.body;
 
     try {
       const profile = await this.profileService.removeUser(userId, profileId);
       return res.status(HttpStatusCode.Ok).json(profile);
-
     } catch (error) {
       next(error);
     }
-  }
+  };
 
-  public postGrant = async (req: Request, res: Response, next: NextFunction): Promise<Response> => { 
+  public postGrant = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response> => {
     const { profileId } = req.params;
     const { grantId } = req.body;
 
@@ -264,32 +314,36 @@ export class ProfileController {
       if (!profile) {
         throw new NotFoundError(`Profile with ID ${profileId} not found.`);
       }
-  
+
       const grant = await grantService.findOne(grantId);
       if (!grant) {
         throw new NotFoundError(`Grant with ID ${grantId} not found.`);
       }
-  
-      const profileUpdated: Profile = await this.profileService.addGrantToProfile(grant, profile);
-  
+
+      const profileUpdated: Profile =
+        await this.profileService.addGrantToProfile(grant, profile);
+
       return res.status(HttpStatusCode.Ok).json(profileUpdated);
     } catch (error) {
       next(error);
     }
-  }
+  };
 
-  public deleteGrant = async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
+  public deleteGrant = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response> => {
     const { profileId } = req.params;
     const { grantId } = req.body;
 
     try {
       const profile = await this.profileService.removeGrant(grantId, profileId);
       return res.status(HttpStatusCode.Ok).json(profile);
-      
     } catch (error) {
       next(error);
     }
-  }
+  };
 }
 
 export default new ProfileController(profileService);
